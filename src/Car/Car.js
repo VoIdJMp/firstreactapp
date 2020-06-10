22cfefd54611ec9f3436c7271139ee4881cd0426
VoIdJMp <59548686+VoIdJMp@users.noreply.github.com>
2020-06-10 11:55:14 +0500

    Commit 2

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,7 +4,7 @@ import './Car.css';
 export default (props) => (
     <div className='mainCars'>
         <h1>{props.name}</h1>
-        <p style={{fontSize: '20px'}}><strong>Год випуска: {props.year}</strong></p>
+        <p style={{fontSize: '20px'}}><strong>Год выпуска: {props.year}</strong></p>
         <input type="text" onChange={props.onChageCar} value={props.name}/>
         <button onClick={props.onDelateCar}> Delate car </button>
     </div>
